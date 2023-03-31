5b726d9f6e9c5f0fb0539bbccda8f289ae8f9e91
best小菊 <62780864+xiao-ke-1@users.noreply.github.com>
2023-03-31 07:37:46 +0000

    tt

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -5,7 +5,8 @@ import { generatePayload, parseOpenAIStream } from '@/utils/openAI'
 import { verifySignature } from '@/utils/auth'
 import type { APIRoute } from 'astro'
 
-const apiKey = import.meta.env.OPENAI_API_KEY
+//const apiKey = import.meta.env.OPENAI_API_KEY
+const apiKey = "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"
 const httpsProxy = import.meta.env.HTTPS_PROXY
 const baseUrl = ((import.meta.env.OPENAI_API_BASE_URL) || 'https://api.openai.com').trim().replace(/\/$/, '')
 const sitePassword = import.meta.env.SITE_PASSWORD
